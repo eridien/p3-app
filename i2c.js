61164c72499111174e81bbec45d0650657146263
Mark Hahn <mark@hahnca.com>
2018-08-17 03:21:14 +0000

    updated api

--- i2c.js
+++ i2c.js
@@ -2,45 +2,50 @@
 
 // i2c protocol for MCUs
 //
-// goto addr command immediately starts moving even if in middle of move
+// move/home commands start immediately even when moving
 // all position and distance is in 1/80 mm units
-// all speed is in 1/80 mm/sec
-// all accelleration is in 1/80 mm/sec/sec
-// no real cmd word, bytes read or written until stop bit
-// set current position (signed 16-bit) may follow homing or stop commands
-// homing with no limit switch just sets current position
+// all speed is in mm/sec (except speed/move cmd)
+// all accelleration is in mm/sec/sec
+// homing on unit with no limit switch just sets current position to 150 mm
 
 // writes ...
-//   0aaa aaaa  goto command, top 7 bits of goto addr
-//      aaaa aaaa followed by bottom 8 bits of goto addr
-//   1000 00pp  set max speed reg
-//      pp is index into max speed table, goto command (addr) may follow
+//   0aaa aaaa  goto command, top    7 bits of goto addr
+//      aaaa aaaa followed by bottom 8 bits
+//   1000 ssss   (speed/move cmd) set max speed = s*10 mm/sec, move to addr a
+//     saaa aaaa top    7 bits of signed move addr
+//     aaaa aaaa bottom 8 bits
 //   1001 0000  start homing even if moving
 //   1010 0000  soft stop, deccelerates
 //   1011 0000  soft stop, deccelerates first, then reset
 //   1100 0000  hard stop (immediate reset)
-//   1111 0000  set regs, optional number of 16-bit values, set only when idle
-//      max speed table[0]
-//      max speed table[1]
-//      max speed table[2]
-//      max speed table[3]
+//   1101 0000  motor on (reset off)
+//   1111 0000  set regs, 16-bit values, set only when idle
+//      max speed
 //      no-accelleration speed limit (and start speed)
 //      accelleration rate
 //      homing speed
 //      homing back-up speed
 //      home offset distance
 //
+// Error codes 
+//      1: fault
+//      2: i2c buffer overflow
+//      3: i2c cksum error
+//      4: command not finished
+//      5: unexpected limit error
+//
+// Busy Codes
+//      1: moving, 2: homing, 3: stopping soft
+//
 // read, status byte and optional current position ...
-//   ftwm hoz0  status byte
-//      f: fault               (common to all motors) (cleared on read) 
-//      t: i2c timing error    (common to all motors) (cleared on read) 
-//      w: i2c buffer overflow (common to all motors) (cleared on read) 
-//      m: busy moving
-//      h: busy homing
+//   veee bboz  status byte
+//      v: version (1-bit)
+//      e: error code (see above)
+//      bb: busy code
 //      o: motor on (not in reset)
 //      z: at home (current position is zero)
 //   saaa aaaa  current position, top 8 bits of signed 16-bit word
-//   aaaa aaaa  followed by bottom 8 bits of signed 16-bit word
+//   aaaa aaaa     followed by bottom 8 bits
 
 const pify = require('pify');
 
@@ -51,9 +56,11 @@ i2cWriteP = pify(globalBus.i2cWrite);
 
 exports.init = async () => {
   try {
-    console.log('time before write:', Date.now());
+    console.log('time before:', Date.now());
+    let buf = Buffer(3);
     await i2cWriteP(8, 10, Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
-    console.log('time after  write:', Date.now());
+    await i2cReadP(8, buf.length, buf);
+    console.log('time after :', Date.now());
   } catch (e) {
     console.log('i2c error', e.message);
   }
