169fbeea94803df2dbad18590f8e6f7e55481622
Mark Hahn <mark@hahnca.com>
2018-09-07 22:07:57 -0700

    moved docs to interface-doc.txt

--- i2c.js
+++ i2c.js
@@ -116,18 +116,19 @@ let motorAddr = {
 }
 
 let opcode = {
-  move:        0x8000,
-  speedMove:   0x4000,
-  startHoming: 0x10,
-  getTestPos:  0x11,
-  softStop:    0x12,
-  softStopRst: 0x13,
-  reset:       0x14,
-  motorOn:     0x15,
-  setHomePos:  0x16,
-  settings:    0x1f,
-};
-
+  move:           0x8000,
+  speedMove:      0x4000,
+  accelSpeedMove: 0x0800,
+  startHoming:    0x10,
+  getTestPos:     0x11,
+  softStop:       0x12,
+  softStopRst:    0x13,
+  reset:          0x14,
+  motorOn:        0x15,
+  setHomePos:     0x16,
+  settings:       0x1f,
+};   
+   
 // this might collide with main loop i2c
 setInterval(chkState, 1000/6);
 
@@ -171,8 +172,8 @@ exports.test = async (pwrSwOnOff) => {
         await i2cWriteP(addr, cmdBuf.byteLength, Buffer.from(cmdBuf));
         await sleep(delay);
       }
-      console.log(getTime(), '============ start homing ============');
-      cmdBuf = setOpcode(opcode.startHoming);
+      console.log(getTime(), '============ reset ============');
+      cmdBuf = setOpcode(opcode.reset);
       await i2cWriteP(addr, cmdBuf.byteLength, Buffer.from(cmdBuf));
     }
   } catch (e) {
@@ -194,89 +195,3 @@ errString = (code) => {
     case 7: return "move cmd when not homed";
   }
 }
-
-// steps are in 1/8 step (bipolar) or one phase (unipolar)
-//    for bipolar:
-//       steps/rev:        1600
-//       dist/rev:           40 mm
-//       max distance:      800 mm
-//       max step count: 32,000
-//
-//    for unipolar:
-//       steps/rev:        2048
-//       dist/rev:           40 mm
-//       max distance:      625 mm
-//       max step count: 32,000
-
-// move/home commands start immediately even when already busy
-// all position and distance is in steps (bi: 1/8 ustep, uni: phase)
-// all speed is in steps/sec (except speed-move cmd)
-// homing with no limit switch just sets current position to settings value
-
-// steps are in 1/8 ustep (bipolar) or one phase (unipolar)
-//    for bipolar:
-//       steps/rev:        1600
-//       dist/rev:           40 mm
-//       max distance:      800 mm
-//       max step count: 32,000
-//
-//    for unipolar:
-//       steps/rev:        2048
-//       dist/rev:           40 mm
-//       max distance:      625 mm
-//       max step count: 32,000
-//
-// writes ...
-//   (first word of recv buffer is buf len)
-//
-//   -- move ommands --
-//   1aaa aaaa  goto command, top 7 bits of goto addr
-//     aaaa aaaa followed by bottom 8 bits
-//   01ss ssss (speed-move cmd) set max speed = s*256 steps/sec and move to addr
-//     0aaa aaaa top 7 bits of move addr
-//     aaaa aaaa bottom 8 bits
-//
-//   -- one-byte commands --
-//   0001 0000  start homing
-//   0001 0001  next read position is end position of homing (test pos)
-//   0001 0010  soft stop, deccelerates, no reset
-//   0001 0011  soft stop, deccelerates first, then reset
-//   0001 0100  hard stop (immediate reset)
-//   0001 0101  motor on (hold place, reset off)
-//   0001 0110  set curpos to home pos value setting (fake homing)
-//
-//   -- 21 byte settings command --
-//   0001 1111  load settings, 16-bit values
-//      acceleration rate table index 0..7 (steps/sec/sec), 0 is off
-//      default speed
-//      no-acceleration speed limit (and start speed when stopped)
-//      max pos     (min pos is always zero))
-//      homing speed
-//      homing back-up speed
-//      home offset distance
-//      home pos value (set cur pos to this value after homing, usually 0)
-//      limit sw control
-
-// -- 4-byte state response --
-// error code and bit cleared on status read, only on motor being read
-// Error codes 
-//   MOTOR_FAULT_ERROR   1
-//   I2C_OVERFLOW_ERROR  2
-//   CMD_DATA_ERROR      3
-//   CMD_NOT_DONE_ERROR  4
-//   STEP_NOT_DONE_ERROR 5
-//   MOTOR_LIMIT_ERROR   6
-//   NOT_HOMED_ERROR     7
-//
-// state response bytes
-//   1) vccc eboz  state byte
-//      v: version (1-bit)
-//    ccc: error code (see above) (only set on specific motor causing error)
-//      e: error bit              (set on all motors when any error in mcu)
-//      b: busy state
-//      o: motor on (not in reset)
-//      z: at home
-//   2) aaaa aaaa  current position, top 8 bits (might be result of cmd 0x11)
-//   3) aaaa aaaa  followed by bottom 8 bits
-//   4) cccc cccc  8-bit cksum, sum of first 3 bytes
-
